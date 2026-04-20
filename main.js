f32cfd2a06158434c72e9918eeb30770807b1830
DeLara9S-ay2526 <delara.xq@obmontessori.edu.ph>
2026-04-21 07:39:14 +0800

    .

---
+++ main.js
@@ -0,0 +1,22 @@
+function openGate() {
+    let carCount = 2;
+    const maxCars = 25;
+    while (carCount < maxCars) {
+        window.alert("Car No. " + carCount + " is entering.");
+        carCount += 1;
+    }
+}
+
+//       C
+//       S
+// N  D  S  M  D
+//       M
+//       L
+
+// V R S
+
+// N S M V
+
+// S M Q L
+
+// I V B
